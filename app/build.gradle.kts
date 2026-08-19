51376919f254f3a93b644a95a6502e5425812ee2
imendjerionerayen-dev <imendjerionerayen@gmail.com>
2026-08-19 21:09:45 +0100

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.logipanel.plc"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.logipanel.plc"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+}
